e0f310989301185e8bd20e70474bb2538a9be9bc
devyinka <217584194+devyinka@users.noreply.github.com>
2026-02-08 03:08:50 +0100

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // 👈 REQUIRED for GitHub Pages
+
   images: {
+    unoptimized: true, // 👈 REQUIRED (next/image not supported on Pages)
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com", pathname: "**" },
       { protocol: "https", hostname: "images.unsplash.com", pathname: "**" },
