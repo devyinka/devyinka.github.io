48a6af97f2f074ae2c9ef989998e12e6fba138f1
devyinka <devyinka8380@gmail.com>
2026-09-06 17:32:00 +0100

    modified:   utils/educationList.ts

--- utils/educationList.ts
+++ utils/educationList.ts
@@ -4,19 +4,12 @@ export const educationList: educationType[] = [
   {
     career: "Full‑Stack & Mobile Developer (Self‑Taught)",
     years: "2023 – Present",
-    description:
-      "Built 5+ production‑ready applications including a real‑time pest and disease monitoring system in bean farm,Multmodal bometric Attendance System. Stack: MERN, React Native, TypeScript, Next.js, Firebase, Socket.io. Designed RESTful APIs with Node.js/Express, implemented JWT authentication, and deployed on Vercel & AWS EC2. Maintained 90%+ test coverage with Jest and React Testing Library.",
+    description: "Built 15+ production‑ready applications",
   },
   {
     career: "B.Eng. in Telecommunication Engineering",
     years: "2021 – 2026",
     description:
       "Federal University of Technology, Minna, Niger State. GPA: 4.2/5.0.",
-  },
-  {
-    career: "Frontend Web Development",
-    years: "2022-till-present",
-    description:
-      "Self-taught in frontend web development, focusing on React, TypeScript, and Next.js. Built responsive and accessible user interfaces, optimized performance, and implemented state management with Redux and Context API.",
   },
 ];
