51ed6d3871c7fb87d48ad1d03fa92819a738e988
devyinka <devyinka8380@gmail.com>
2026-09-09 03:54:10 +0100

    modified:   utils/educationList.ts

--- utils/educationList.ts
+++ utils/educationList.ts
@@ -2,16 +2,15 @@ import { educationType } from "@/types";
 
 export const educationList: educationType[] = [
   {
-    title: "Telecommunication Engineering (B.Eng)",
-    subtitle: "Federal University of Technology, Minna (2021 – 2026)",
-    description:
-      "Graduated with a 4.2 CGPA, developing a strong foundation in computer networks, communication systems, embedded systems, and connected technologies.",
-  },
-
-  {
     title: "Full-Stack & Mobile Developer",
     subtitle: "Self-Taught & Industrial Training (2023 – Present)",
     description:
       "Developed practical skills in React, Next.js, Node.js, Express, React Native, and database-driven applications.",
   },
+  {
+    title: "Telecommunication Engineering (B.Eng)",
+    subtitle: "Federal University of Technology, Minna (2021 – 2026)",
+    description:
+      "Graduated with a 4.2 CGPA, developing a strong foundation in computer networks, communication systems, embedded systems, and connected technologies.",
+  }
 ];
